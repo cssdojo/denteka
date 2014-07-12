a671c32fdb4b8dcefbb6f8b9cce25ca40e059644
Denis Pismenny <denis.pismenny@gmail.com>
2014-07-12 16:02:36 +0300

    index + fonts

--- js/common.js
+++ js/common.js
@@ -1,8 +1,25 @@
 head.ready(function() {
 
-	$(' .subnav .has-dropdown ').on('click', function() {
-		$(this).toggleClass('is-active');
+	$(' .subnav .has-dropdown > a ').on('click', function() {
+		$('.search__form').slideUp('fast');
+		$(this).parent().toggleClass('is-active');
 		return false;
+	});
+
+	$('.search__ordertrigger').on('click', function() {
+		$('.subnav .has-dropdown').removeClass('is-active');
+		$(this).parent().find('.search__form').slideToggle('fast');
+		return false;
+	});
+
+	$(document).click(function(event) {
+		// $('.js-select').removeClass('is-open');
+		if($(event.target).parents().index($('.subnav')) == -1) {
+			$('.subnav .has-dropdown').removeClass('is-active');
+		}
+		if($(event.target).parents().index($('.search__order')) == -1) {
+			$('.search__form').slideUp('fast');
+		}
 	});
 
 });
